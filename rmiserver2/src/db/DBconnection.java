89b43bab15d9bfeaf273aedbe727fdde972671e3
dimesha <dimesha99kanchani@gmail.com>
2019-07-01 13:18:38 +0530

    fifth commit

--- rmiserver2/src/db/DBconnection.java
+++ rmiserver2/src/db/DBconnection.java
@@ -5,10 +5,34 @@
  */
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author dimesha
  */
 public class DBconnection {
+private static DBconnection dBconnection;
+private Connection connection;
+   private DBconnection() {
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+      
+    } catch (ClassNotFoundException ex) {
+        Logger.getLogger(DBconnection.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    }
+   public  static DBconnection getinstance(){
+       if (dBconnection==null) {
+           dBconnection=new DBconnection();
+       }
+       return dBconnection;
+   } 
     
+   public Connection getcoConnection(){
+       return connection;
+   }
 }
